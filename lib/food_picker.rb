e1fe7e7b07f85365e9c3064240619498005d114b
Reuben Hillyer <reuben.hillyer@hotmail.co.uk>
2023-11-23 18:19:57 +0000

    Add shameless green for diner options

--- lib/food_picker.rb
+++ lib/food_picker.rb
@@ -23,4 +23,20 @@ class FoodPicker
             "Elves will have a plate of syrup spaghetti served with a glass of soda.\n"
         end
     end
+
+    def dinerMenuOptions(diner) 
+        if (diner == "Santa")
+            "Santa's healthy option is 3 sprouts served with a glass of water.\n" +
+            "Santa's standard option is 1 mince pie served with a measure of brandy.\n" +
+            "Santa's gourmet option is 1 slice of fruit cake and cream served with a glass of mulled apple juice.\n"
+        elsif (diner == "Rudolph")
+            "Rudolph's healthy option is 1 carrot served with a glass of milk.\n" +
+            "Rudolph's standard option is a bag of reindeer food served with a cup of hot chocolate.\n" +
+            "Rudolph's gourmet option is 1 slice of carrot cake served with a glass of festive cosmopolitan.\n"      
+        elsif (diner == "Elves")
+            "Elves's healthy option is some fairy dust served with a glass of milk.\n" +
+            "Elves's standard option is a plate of syrup spaghetti served with a glass of soda.\n" +
+            "Elves's gourmet option is a plate of syrup spaghetti and extra syrup served with a glass of soda.\n"
+        end      
+    end
 end
--- test/food_picker_test.rb
+++ test/food_picker_test.rb
@@ -50,7 +50,7 @@ class FoodPickerTest < Minitest::Test
     assert_equal expected, FoodPicker.new.fullMenuOptions()
   end
 
-  def skip test_the_menu_for_santa
+  def test_the_menu_for_santa
     expected = 
       "Santa's healthy option is 3 sprouts served with a glass of water.\n" +
       "Santa's standard option is 1 mince pie served with a measure of brandy.\n" +
@@ -59,7 +59,7 @@ class FoodPickerTest < Minitest::Test
     assert_equal expected, FoodPicker.new.dinerMenuOptions("Santa")
   end
 
-  def skip test_the_menu_for_rudolph
+  def test_the_menu_for_rudolph
     expected = 
       "Rudolph's healthy option is 1 carrot served with a glass of milk.\n" +
       "Rudolph's standard option is a bag of reindeer food served with a cup of hot chocolate.\n" +
@@ -68,7 +68,7 @@ class FoodPickerTest < Minitest::Test
     assert_equal expected, FoodPicker.new.dinerMenuOptions("Rudolph")
   end
 
-  def skip test_the_menu_for_elves
+  def test_the_menu_for_elves
     expected = 
       "Elves's healthy option is some fairy dust served with a glass of milk.\n" +
       "Elves's standard option is a plate of syrup spaghetti served with a glass of soda.\n" +
