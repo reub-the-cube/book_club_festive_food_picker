fe6ab3550c29b9ef2044232f4bf5212c8d8c5d66
Reuben Hillyer <reuben.hillyer@hotmail.co.uk>
2023-11-24 16:29:38 +0000

    Work on refactoring by flocking rules for the menu options. End of book club session for chapter 4.

--- lib/food_picker.rb
+++ lib/food_picker.rb
@@ -6,37 +6,67 @@ class FoodPicker
     end
     
     def menuOptions(gastronomy) 
+        "The #{gastronomy} menu:\n" +
+        "Santa will have #{santaFood(gastronomy)} served with #{santaDrink(gastronomy)}.\n" +
+        "Rudolph will have #{rudolphFood(gastronomy)} served with #{rudolphDrink(gastronomy)}.\n" +
+        "Elves will have #{elfFood(gastronomy)} served with #{elfDrink(gastronomy)}.\n"
+    end
+
+    def santaFood(gastronomy)
         if (gastronomy == "healthy")
-            "The #{gastronomy} menu:\n" +
-            "Santa will have 3 sprouts served with a glass of water.\n" +
-            "Rudolph will have 1 carrot served with a glass of milk.\n" +
-            "Elves will have some fairy dust served with a glass of milk.\n"
+            "3 sprouts"
         elsif (gastronomy == "gourmet")
-            "The #{gastronomy} menu:\n" +
-            "Santa will have 1 slice of fruit cake and cream served with a glass of mulled apple juice.\n" +
-            "Rudolph will have 1 slice of carrot cake served with a glass of festive cosmopolitan.\n" +
-            "Elves will have a plate of syrup spaghetti and extra syrup served with a glass of soda.\n"
+            "1 slice of fruit cake and cream"
         else
-            "The #{gastronomy} menu:\n" +
-            "Santa will have 1 mince pie served with a measure of brandy.\n" +
-            "Rudolph will have a bag of reindeer food served with a cup of hot chocolate.\n" +
-            "Elves will have a plate of syrup spaghetti served with a glass of soda.\n"
+            "1 mince pie"
         end
     end
 
-    def dinerMenuOptions(diner) 
-        if (diner == "Santa")
-            "Santa's healthy option is 3 sprouts served with a glass of water.\n" +
-            "Santa's standard option is 1 mince pie served with a measure of brandy.\n" +
-            "Santa's gourmet option is 1 slice of fruit cake and cream served with a glass of mulled apple juice.\n"
-        elsif (diner == "Rudolph")
-            "Rudolph's healthy option is 1 carrot served with a glass of milk.\n" +
-            "Rudolph's standard option is a bag of reindeer food served with a cup of hot chocolate.\n" +
-            "Rudolph's gourmet option is 1 slice of carrot cake served with a glass of festive cosmopolitan.\n"      
-        elsif (diner == "Elves")
-            "Elves's healthy option is some fairy dust served with a glass of milk.\n" +
-            "Elves's standard option is a plate of syrup spaghetti served with a glass of soda.\n" +
-            "Elves's gourmet option is a plate of syrup spaghetti and extra syrup served with a glass of soda.\n"
-        end      
+    def santaDrink(gastronomy)
+        if (gastronomy == "healthy")
+            "a glass of water"
+        elsif (gastronomy == "gourmet")
+            "a glass of mulled apple juice"
+        else
+            "a measure of brandy"
+        end
     end
-end
+
+    def rudolphFood(gastronomy)
+        if (gastronomy == "healthy")
+            "1 carrot"
+        elsif (gastronomy == "gourmet")
+            "1 slice of carrot cake"
+        else
+            "a bag of reindeer food"
+        end
+    end
+
+    def rudolphDrink(gastronomy)
+        if (gastronomy == "healthy")
+            "a glass of milk"
+        elsif (gastronomy == "gourmet")
+            "a glass of festive cosmopolitan"
+        else
+            "a cup of hot chocolate"
+        end
+    end
+
+    def elfFood(gastronomy)
+        if (gastronomy == "healthy")
+            "some fairy dust"
+        elsif (gastronomy == "gourmet")
+            "a plate of syrup spaghetti and extra syrup"
+        else
+            "a plate of syrup spaghetti"
+        end
+    end
+
+    def elfDrink(gastronomy)
+        if (gastronomy == "healthy")
+            "a glass of milk"
+        else
+            "a glass of soda"
+        end
+    end
+end     
